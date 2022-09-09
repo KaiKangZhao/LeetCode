9f0ee6bbe26228337b6dbe878359295aada83311
zkk <zhaokaikang@seu.edu.cn>
2022-09-09 20:25:32 +0800

    回溯

--- backtracking/src/combine/LeetCode77/Solution2.java
+++ backtracking/src/combine/LeetCode77/Solution2.java
@@ -1,18 +1,18 @@
 package combine.LeetCode77;
 
-/**
- * @author zkk
- * @date 2022/9/9 19:55
- */
-
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 /**
- * 给定两个整数 n 和 k，返回 1 ... n 中所有可能的 k 个数的组合。
+ * 剪枝优化已经选择的元素个数：path.size();
+ * 还需要的元素个数为: k - path.size();
+ * 在集合n中至多要从该起始位置 : n - (k - path.size()) + 1，开始遍历
+ *
+ * @author zkk
+ * @date 2022/9/9 20:12
  */
-class Solution {
+public class Solution2 {
     // 存放符合条件结果的集合
     List<List<Integer>> result = new ArrayList<>();
     //用来存放符合条件结果
@@ -21,20 +21,16 @@ class Solution {
     public List<List<Integer>> combine(int n, int k) {
         backtrack(n, k, 1);
         return result;
-
     }
 
-    public void backtrack(int n, int k, int startIndex) {
-        if (path.size() == k) {
+    void backtrack(int n, int k, int startIndex) {
+        if (path.size()==k){
             result.add(new ArrayList<>(path));
             return;
         }
-        for (int i = startIndex; i <= n; i++) {
-            //处理节点
+        for (int i = startIndex; i <=n-(k- path.size())+1; i++) {
             path.add(i);
-            //递归
-            backtrack(n, k, i + 1);
-            // 回溯，撤销处理的节点
+            backtrack(n, k, i+1);
             path.removeLast();
         }
     }
