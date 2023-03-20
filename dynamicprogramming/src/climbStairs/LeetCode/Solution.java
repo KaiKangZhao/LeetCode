83c3d3f7bce05768ca1c0474cd8db5b8b1ccb9cd
zkk <zhaokaikang@seu.edu.cn>
2023-03-20 11:25:00 +0800

    动态规划

---
+++ dynamicprogramming/src/climbStairs/LeetCode/Solution.java
@@ -0,0 +1,23 @@
+package climbStairs.LeetCode;
+
+/**
+ * @author zkk
+ * @Date 2023/3/17 15:33
+ */
+
+/**
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ * 注意：给定 n 是一个正整数。
+ */
+public class Solution {
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 2];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+}
